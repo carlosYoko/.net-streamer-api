2a734b6d51e3f13f3756a3307d1e736fd117216a
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-05-12 12:30:05 +0200

    Add project files.

---
+++ CleanArchitecture.Domain/Streamer.cs
@@ -0,0 +1,12 @@
+﻿namespace CleanArchitecture.Domain
+{
+    internal class Streamer
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Url { get; set; } = string.Empty;
+
+    }
+}
---
+++ CleanArchitecture.Domain/Video.cs
@@ -0,0 +1,13 @@
+﻿namespace CleanArchitecture.Domain
+{
+    internal class Video
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int StramerId { get; set; }
+
+        public virtual Streamer Streamer { get; set; }
+    }
+}
